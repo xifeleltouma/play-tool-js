7f46cc7e89fb9730aa8f014efa72d3a9b21fa79e
xifeleltouma <229006387+xifeleltouma@users.noreply.github.com>
2025-09-11 17:43:40 +0200

    fix vscode config for prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,17 @@
-import js from "@eslint/js"
-import globals from "globals"
-import eslintConfigPrettier from "eslint-config-prettier"
-import { defineConfig } from "eslint/config"
+import js from '@eslint/js'
+import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: { ...globals.node, ...globals.browser } },
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
     rules: {
       ...js.configs.recommended.rules,
     },
